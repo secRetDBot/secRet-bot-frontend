15bbc2c1884e1a900bf2d251850b57b99cadc6c4
iGio90 <giovanni.rocca.90@gmail.com>
2018-01-26 13:25:56 +0100

    update labels

--- static/app.js
+++ static/app.js
@@ -4,32 +4,7 @@ let app = new Vue({
     el: '#app',
     data: {
         menuVisible: false,
-        cards: [{test: "omg", blah: "aaa"}, {test: "lol", blah: "bbb"}],
-
         bot: {
-            "bot_status": {
-                "now": "23:30:19",
-                "uptime": "0:00:05",
-                "icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Icons8_flat_clock.svg/2000px-Icons8_flat_clock.svg.png"
-            },
-            "git_status": {
-                "updated": "2018-01-22 18:36:22",
-                "status": "good",
-                "icon": "https://avatars3.githubusercontent.com/u/1153419?s=400&v=4"
-            },
-            "mongo_status": {
-                "host": "igio90-desktop",
-                "version": "3.4.7",
-                "process": "mongod",
-                "pid": 921,
-                "db": "secret",
-                "collections": 3,
-                "objects": 340,
-                "indexes": 7,
-                "storage_size": 118784,
-                "index_size": 204800,
-                "icon": "https://www.todobackend.com/images/logos/mongodb.png"
-            }
         }
     },
 
